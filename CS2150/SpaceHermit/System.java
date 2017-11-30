9272f16c6d75eef42a727b645b8ce393af33917b
Matthew Barber <barberm1@aston.ac.uk>
2017-11-30 12:18:03 +0000

    some refactoring, more work on system generator

--- CS2150/SpaceHermit/System.java
+++ CS2150/SpaceHermit/System.java
@@ -1,9 +1,11 @@
 package SpaceHermit;
 
+import java.util.List;
 import java.util.Random;
 
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.Sphere;
+import org.newdawn.slick.opengl.Texture;
 
 public class System {
 	private Random rnd = new Random();
@@ -20,6 +22,18 @@ public class System {
 	private float x;
 	private float y;
 	
+	//TODO path values to access skyboxes
+	private String pckgDir = "SpaceHermit";
+	private String textureDir = "skyboxes";
+	private String[] plantTextureNames = { "corona_ft.png", "redeclipse_ft.png",
+			"unnamedspace_ft.jpg", "unnamedspace3_ft.png" };
+	private String[] planetTextureNames = { "corona_ft.png", "redeclipse_ft.png",
+			"unnamedspace_ft.jpg", "unnamedspace3_ft.png" };
+	// stores loaded planet textures
+	private List<Texture> textures;
+	// stores planet texture currently in use
+	private Texture currentTexture;
+	
 	private Sphere planet;
 	private Moon[] moons;
 	
@@ -28,6 +42,7 @@ public class System {
 		this.distMin = distMin;
 		this.distMax = distMax;
 		newSystem();
+		textures = Util.loadTextures(pckgDir + "/" + textureDir, planetTextureNames);
 	}
 	
 	public void updateScene() {
@@ -35,18 +50,31 @@ public class System {
 	}
 	
 	public void renderScene() {
-		GL11.glTranslatef(0.0f, 0.0f, dist);
-		planet.draw(size, 16, 16);
-		//for(Moon moon: moons) {
-			//
-		//}
+		drawPlanet();
 	}
 	
 	public void newSystem() {
 		size = rnd.nextFloat() * (sizeMax - sizeMin);
 		dist = rnd.nextFloat() * ((distMax + size * 2) - (distMin - size * 2));
 		planet = new Sphere();
+	}
+	
+	private void drawPlanet() {
+		/* reset scaling and rotation */
+		GL11.glScalef(1.0f, 1.0f, 1.0f);
+		GL11.glRotatef(0.0f, 0.0f, 0.0f, 0.0f);
+
+		/* set position of planet */
+		GL11.glTranslatef(0.0f, 0.0f, dist);
 		
+		/* set material properties */
+		float shininess = 1.0f;
+		float[] specular = { 1.0f, 1.0f, 1.0f, 1.0f };
+		float[] colour = { 0.75f, 0.75f, 0.75f, 1.0f };
+
+		Util.material(shininess, specular, colour);
+		
+		planet.draw(size, 16, 16);
 	}
 	
 	
